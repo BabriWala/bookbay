58232cc904bf52b528f16213994dac7aa6c7a62a
Muhammad Hanzala <91691179+BabriWala@users.noreply.github.com>
2025-04-17 23:04:45 +0600

    Update typography, iconify and header component

---
+++ src/components/Iconify/Iconify.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+const Iconify = ({ icon }) => {
+  return <Icon icon={icon}></Icon>;
+};
+
+export default Iconify;
---
+++ src/pages/TestComponents/TestComponents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const TestComponents = () => {
+  return (
+    <div className="container mx-auto my-20 shadow">
+      <Header></Header>
+    </div>
+  );
+};
+
+export default TestComponents;
